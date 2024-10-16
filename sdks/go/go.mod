cc8bf86bad527370c0511717bd3874a60400f662
schmidtw <Weston_Schmidt@alumni.purdue.edu>
2024-10-16 08:54:36 -0700

    Fix the rbus handle so it compiles as a starting point.

--- sdks/go/go.mod
+++ sdks/go/go.mod
@@ -2,11 +2,9 @@ module github.com/schmidtw/rbus-rdk/sdks/go/rbus
 
 go 1.22.0
 
-require github.com/xmidt-org/eventor v1.0.18
-
 require (
-	github.com/k0kubun/pp v3.0.1+incompatible // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.16 // indirect
-	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	github.com/vmihailenco/msgpack/v5 v5.4.1
+	github.com/xmidt-org/eventor v1.0.18
 )
+
+require github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
--- sdks/go/rbus.go
+++ sdks/go/rbus.go
@@ -4,9 +4,7 @@
 package rbus
 
 import (
-	"context"
 	"errors"
-	"fmt"
 
 	"github.com/schmidtw/rbus-rdk/sdks/go/rbus/rtmessage"
 )
@@ -54,12 +52,12 @@ func New(opts ...Option) (*Handle, error) {
 
 // Open creates a new rbus connection or returns an error.
 func (h *Handle) Open() error {
-	con, err := rtmessage.New(h.cfg.url, h.cfg.appName)
+	con, err := rtmessage.New(h.cfg.url, h.cfg.appName, h.cfg.id)
 	if err != nil {
 		return err
 	}
 
-	err = con.Connect(h.messageHandler)
+	err = con.Connect()
 	if err != nil {
 		return err
 	}
@@ -68,61 +66,8 @@ func (h *Handle) Open() error {
 	return nil
 }
 
-func (h *Handle) messageHandler(header *rtmessage.Header, payload []byte) {
-	msg, err := NewMessageFromBytes(payload)
-	if err != nil {
-		fmt.Printf("Failed to create message from bytes. %s\n", err.Error())
-	} else {
-    // TODO: wrap the following up in some type of read
-    // the problem is that different rbus messages have different
-    // msgpack structures
-		returnCode, err := msg.PopInt32()
-		if err != nil {
-			panic(fmt.Sprintf("Failed to pop int32 for return code. %s\n", err.Error()))
-		}
-
-		valueSize, err := msg.PopInt32()
-		if err != nil {
-			panic(fmt.Sprintf("Failed to pop int32 for value size. %s\n", err.Error()))
-		}
-
-		parameterName, err := msg.PopString()
-		if err != nil {
-			panic(fmt.Sprintf("Failed to pop string. %s\n", err.Error()))
-		}
-
-		fmt.Printf("Return code: %d\n", returnCode)
-		fmt.Printf("Value size: %d\n", valueSize)
-		fmt.Printf("Parameter name: '%s'\n", parameterName)
-
-		value, err := msg.PopValue()
-		if err != nil {
-			panic(fmt.Sprintf("Failed to pop value. %s\n", err.Error()))
-		}
-
-		fmt.Printf("%s == %s\n", parameterName, value.String())
-
-	}
-}
-
-func (h *Handle) Get(ctx context.Context, name string) (*Value, error) {
-	if h.conn == nil {
-		return nil, errors.New("connection not open")
-	}
-
-	msg := NewMessage()
-	msg.AppendString(h.cfg.appName)
-	msg.AppendInt32(1)
-	msg.AppendString(name)
-	msg.SetMetaInfo("METHOD_GETPARAMETERVALUES", "todo_openTelemetry_parent", "todo_openTelemetry_state")
-
-	if err := h.conn.SendRequest(ctx, msg.Bytes(), name); err != nil {
-		return nil, err
-	}
-
-	// TODO: what do we do now? Where does user get result
-
-	return nil, nil
+func (h *Handle) Get(name string) (*Value, error) {
+	return nil, errors.New("not implemented")
 }
 
 func (h *Handle) Set(name string, value *Value) error {
